5b4a3308aba9a71ce085b7f77d2f06e83f4d7f48
Senghab-Cheng <scheng@paragoniu.edu.kh>
2024-09-02 15:41:08 +0700

    dhdsjh

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 5, b = 10;
+    int sum = a + b;
+
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+
+    return 0;
+}
